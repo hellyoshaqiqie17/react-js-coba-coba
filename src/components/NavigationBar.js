3d78917856fdd0671e1ad96ec78955992b3cadbd
Hellyoshaqiqie <77888513+hellyoshaqiqie17@users.noreply.github.com>
2023-03-05 14:04:39 +0700

    Update NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,7 @@ const NavigationBar = () => {
     <div>
       <Navbar variant="dark">
         <Container>
-          <Navbar.Brand href="/">DEAFILMS</Navbar.Brand>
+          <Navbar.Brand href="/">HQQFILMS</Navbar.Brand>
           <Nav>
             <Nav.Link href="#trending">TRENDING</Nav.Link>
             <Nav.Link href="#superhero">SUPERHERO</Nav.Link>
